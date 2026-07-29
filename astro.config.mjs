8a6b907d407fa2277850b704e4d27d509c50f8fd
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-07-29 11:12:08 -0500

    updated everything and added SEO content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,26 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
+
 export default defineConfig({
+  trailingSlash: 'always',
+
+  build: {
+    format: 'directory', // Ensures pages are built as /page/index.html
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  server: {
+    port: 3000,
+    open: true, 
+  },
+
+  integrations: [sitemap()],
+
+  site: 'https://cflmc.org/',
 });
